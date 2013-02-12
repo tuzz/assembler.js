b7d0fa59c3f0f429e3a4a9a7ef5f6c8b9c850756
Chris Patuzzo <chris@patuzzo.co.uk>
2013-02-12 23:01:18 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## assembler.js
+
+A JavaScript assembler for the [Hack assembly language](http://www.nand2tetris.org/06.php).
+
+## Setup
+
+```
+brew install v8
+```
+
+## Usage
+
+```
+d8 assembler.js -- file.asm
+```
---
+++ assembler.js
@@ -0,0 +1,15 @@
+load("lib/io.js");
+
+Assembler = {};
+
+Assembler.assemble = function () {
+  var input = IO.read();
+
+  ///// Implementation pending.
+  var output="01010101010101";
+  /////
+
+  IO.write(output);
+};
+
+Assembler.assemble();
